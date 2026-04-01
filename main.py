60457d4d73f2e08a3b83d7c31cd70115ffa2b14b
korawitsriratpak-glitch <korawit.srirat.pak@gmail.com>
2026-04-01 18:22:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from fastapi import FastAPI
-from typing import Dict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-db: Dict[str, dict] = {}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def home():
-    return {"status": "SCADA API Running"}
+data_store = {}
 
-@app.post("/update")
-def update(data: dict):
+@app.post("/data")
+def receive_data(data: dict):
     machine = data["machine"]
-    db[machine] = data
+    data_store[machine] = data
     return {"status": "ok"}
 
 @app.get("/data")
 def get_data():
-    return db
+    return data_store
