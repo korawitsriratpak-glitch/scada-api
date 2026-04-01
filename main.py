e589672b6bb3e97034dfd798815bc8343e5a4272
korawitsriratpak-glitch <korawit.srirat.pak@gmail.com>
2026-04-01 14:39:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ db: Dict[str, dict] = {}
 
 @app.get("/")
 def home():
-return {"status": "SCADA API Running"}
+    return {"status": "SCADA API Running"}
 
 @app.post("/update")
 def update(data: dict):
-machine = data["machine"]
-db[machine] = data
-return {"status": "ok"}
+    machine = data["machine"]
+    db[machine] = data
+    return {"status": "ok"}
 
 @app.get("/data")
 def get_data():
-return db
+    return db
