78af14e3041799120bb24ed1d44160400b78b737
korawitsriratpak-glitch <korawit.srirat.pak@gmail.com>
2026-04-01 13:45:38 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from typing import Dict
+
+app = FastAPI()
+
+db: Dict[str, dict] = {}
+
+@app.get("/")
+def home():
+return {"status": "SCADA API Running"}
+
+@app.post("/update")
+def update(data: dict):
+machine = data["machine"]
+db[machine] = data
+return {"status": "ok"}
+
+@app.get("/data")
+def get_data():
+return db
