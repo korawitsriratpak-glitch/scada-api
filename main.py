41d3a9a3f0340b477ed7ea3ed8bbf80ccadf8bb4
korawitsriratpak-glitch <korawit.srirat.pak@gmail.com>
2026-04-03 20:57:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -40,10 +40,10 @@ def get_history(machine: str = "Piller1"):
 
     query = f"""
     from(bucket: "{bucket}")
-      |> range(start: -12h)
+      |> range(start: -24h)
       |> filter(fn: (r) => r._measurement == "scada_tef4")
       |> filter(fn: (r) => r._field == "pressure")
-      |> filter(fn: (r) => r.machine == "{machine}")
+      |> filter(fn: (r) => r["machine"] == "{machine}")
       |> sort(columns: ["_time"])
     """
 
